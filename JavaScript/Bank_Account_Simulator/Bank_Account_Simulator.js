e35f72fa853b76ee50187de0438825eebd042680
Sameera Kuna <sameera@gsstech.in>
2026-03-02 11:15:43 +0530

    Delete JavaScript/Bank_Account_Simulator directory

--- JavaScript/Bank_Account_Simulator/Bank_Account_Simulator.js
+++
@@ -1,8 +0,0 @@
-// Bank Account Simulator 
-// Operations: 
-// deposit(amount) 
-// withdraw(amount) 
-// checkBalance() 
-// Balance updates: 
-// newBalance = balance + amount 
-// newBalance = balance − amount 
